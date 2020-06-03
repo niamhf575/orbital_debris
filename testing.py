6f8a694e7c36641d5228a3677704dfc87c2905fe
Aaron Abeyta <aaronabeyta97@gmail.com>
2020-06-03 14:08:39 -0700

    Tests for Calcs

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import data_analysis
+import calculations as calcs
 from data_processing import process_data
-import data_viz
+# import data_viz
 from utils import assert_equals
 
 def  test_data_processing(data_file):
@@ -11,7 +12,7 @@ def  test_data_processing(data_file):
     columns used in our analysis is correct 
     """
     data = process_data(data_file)
-    print(data)
+
     assert_equals(.0092997, data['Eccentricity'].loc[0])
     assert_equals(.0005588, data['Eccentricity'].loc[1])
     assert_equals('62039A', data['InternationalDesignator'].loc[0])
@@ -22,9 +23,21 @@ def  test_data_processing(data_file):
     assert_equals(58.3055, data['OrbitInclination'].loc[1])
     
 
+def test_calcs(data_file):
+
+    data = process_data(data_file)
+    calc_df = calcs.probability_calc(data)
+    semimajor_df = calcs.semimajor_calc(data)
+    relevant = calc_df[calc_df['SatelliteCatalogNumber'] == 614]
+
+    print(relevant)
+
+    assert_equals(7072.035, relevant['SemiMajorAxis'])
+    assert_equals(0.000000302, relevant['Probability'])
 
 def main():
     test_data_processing('test_process_data.txt')    
+    test_calcs('test.txt')
 
 if __name__ == '__main__':
     main()
